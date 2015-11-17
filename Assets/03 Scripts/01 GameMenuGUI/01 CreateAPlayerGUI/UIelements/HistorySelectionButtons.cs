93734e7ab62580f60cdea4547a5a59a0ff428f33
Rgallouet <rgallouet@gmail.com>
2015-11-18 00:40:28 +0100

    Added Visualisation of character (2D) and choice history display

--- Assets/03 Scripts/01 GameMenuGUI/01 CreateAPlayerGUI/UIelements/HistorySelectionButtons.cs
+++ Assets/03 Scripts/01 GameMenuGUI/01 CreateAPlayerGUI/UIelements/HistorySelectionButtons.cs
@@ -14,19 +14,20 @@ public class HistorySelectionButtons : MonoBehaviour {
 	public static RectTransform HistoChoiceDescription;
 
 	public static Button[] Choice = new Button[10];
+	public static Text[] HistoryChoiceDisplay = new Text[8];
 
 
 	public static string[] HellCircles = new string[9] 
 	{"Limbo town","Lust hills","Gluttonbourg","Greedpolis","Capanger","Heresynia","Violocanto","Bolgiafraudis","Traitor's frostgrounds"};
 
 	public static string[] HellCirclesDescription = new string[9]
-	{"Limbo town is the capital of the first hell circle. Filled with brillant minds, pacifists and scholars of chaos, the cosmopolite Limbo town is considered as the cultural and knowledge center of hell. Situated in the most remote circle, it benefit from a decent gravity but a cold climate with a dim vegetal life. The Senate, an immaculate, transparent pentagram building, made of polished diamond towers, is its decision making organ. The Senate, opened to all, often heed the advice of unofficial triumvirate : Yrsif, Oug and Sutin with no affiliation to the great demon houses. \nPopulation: Limbo dwellers prefer cooperation and studying the non-physical perks of their existence.",
+	{"Limbo town is the capital of the first hell circle. Filled with brillant minds, pacifists and scholars of chaos, the cosmopolite Limbo town is considered as the cultural and knowledge center of hell. \nSituated in the most distant circle, it benefits from a low gravity, cold climate and a dim vegetal life. \nThe Senate, an immaculate, transparent pentagram building, made of polished diamond towers, is its decision making organ. Open to all, the Senate often heed the advice of its unofficial triumvirate : Yrsif, Oug and Sutin, with no affiliation to any great demon houses. \nPopulation: Limbo dwellers prefer cooperation and studying the non-physical perks of their existence.",
 		"Ah...The windy halls of lust... a sight of wonder renowned in all hell circles. home habitat of the great flying jellyfish of the thousands red silk threads, The silvery sky can hardly be seen due to the swarm of dark red hair-looking threads hanging around. The land is abusively called halls, but only a few pillars of black marble, hundreds of meters high, are lost on an otherwise red scenery of hills. The flying jellyfish have an ever-lasting mating season, and the hills are endlessly covered with a thick layer of... juicy... wiggly... glue-like red jellyfish eggs. Broken eggs emit myst and strong chemicals that tends to disrupt inhibition function of any species known in hell. Owned by the Incubus congregation, overseer Harpoc from the house Hardy, established an hourly fee for any demons entering the region to reduce crowdness.\nPopulation: Dwellers appreciate contemplation and ... well... chemically induced physical activity...",
 		"Gluttonbourg is a suburban town of the Gluttonia Megapolis, in the last region able to arbor both vegetal and animal life. Dry mud roads, little rocky houses and bushy gardens can confuse any sane-minded demon: this city entertained the human dream to its littlest details. Its mayor, Lord Butchy, is a remote member of the powerful Di Oprescu family famous for its key role in the war on the Navates species, and his finances are largely responsible for the town's architectural mimicry perfection. Quiet town with no particular history except its architecture, It is well known for its MOREMEAT burgers (mashed merguez, oyster shell, reindeer ribs, eagle's talons, marmot cheeks, eel, albatross filet and tapir based patty). \nPopulation: After eating that everyday, anyone can withstand pretty much anything",
 		"Don Papa, War intendant Darras, Elvish duke Dinu l'escou, so many child demon stories that paved the way for grandeur and the rise of the bourgeoisie. Heroes of wealth, all born and raised in the underground city of Greedopolis. Cavernous dark alleys here and there on an otherwise smoky and hectic country-sized bazaar, the city is a lethal maze for any untrained adventure. Shiny boards of a million light pave the lack of sky, sparsely interrupted by casinos so high they litteraly dig back in the earth from which they rise. Greedpolis is undisputedly the oppulant Hell business center.\nClose your bags, brace yourself to the heavy gravity, and look for enemies everywhere... \nPopulation: surprise effect and independance is the only survival factor.",
 		"Grey as cinder, spicky fences, edgy houses, Capanger is a silent haven for demons, situated on the edge of the DeepHollow cliff. The vast emptyness a few metters away, from which no light or sound seem to ever escape, never takes rests in a demon's mind. The sense of gravity, of time, seems always distorted in its viscinity, and an invisible, relentless, yet inexplicable feeling of pull is described by many of its inhabitants. The city is often described as being rules by the heavy emptiness that resides in its every corner.\nPopulation:Passive aggresivity is a form of politeness and the occasional heat storms tend to harden the fallen souls of the area.",
-		"In the majestic swamp of Heri, a complex constellation of wooden streets and dry mud back alleys can hardly be seen in the constant heavy greenish fog. The whole city slowly moves, swim in the hungry swamp, curls who moves, rotates, curls as he pleases in his undergound swamp.\nHeriens learn to adapt their inner beliefs on a daily basis, which is easier when not having to debate on those with your moving neighbors.",
-		"Violocanto, the desolate region dominated by the three concentric alluminium volcanos, is but sparsely populated. \n ",
+		"In the gigantic swamp of Heri, a complex constellation of wooden streets and dry mud back alleys can be spotted in the heavy greenish fog. The whole city slowly moves, swims in the hungry swamp, curls, rotates on the whims of the ancient Kraken on which it resides. The Gall Pak architect family strangely survived centuries of submersion or challenging reversal of the city, and rule it under highly permisive rules: which is having none. \nPopulation: Heriens learn to change their inner beliefs on a daily basis, which is easier when not having to debate on those with their daily new neighbors.",
+		"Violocanto, the desolate region dominated by the three concentric volcanos erupting massive flows of alluminium and silver, is but sparsely populated. Violocanto can be found in Violanis, the first hell circle that host no other life than demons due to its proximity to the hell center. Water naturaly exists only as a gaz form which tends to complicate fancy life styles. The land is ruled by the nomadic Ani Tribe, merchant of water. \nPopulation: living in Violocanto requires a few thing: a strong resistence to elements, and a deft hand for 'picking up' unattended vials of water.",
 		"Bolgiafraudis, also called the Red Fan, is an impressive vertical composition of ten vast castles made of copper, gold and red rare stones, dominating a barren desert ground. High gravity, hot and dry winds, barren waste lands and occasional acid lakes are the only features one can expect here outside the city. Ruled over by the rust dragon Maliborg and his family, the stronghold is the official Deministry of War Waging in times of invasions. Many of the highest and strongest house of hell resides in the top layers of the Red Fan. The regional laws follow the strong sense of hierarchy and military disposition put in place by the last dead dragon. \nPopulation: The highdead demons are trained to battle for invasion of physical worlds, by mastering non-physical offence skills and physical defence.",
 		"The frost grounds lost its original name and purpose in times before any current walking species arrived in the hell circles. It is the closest circle, and against all expectation by far the coldest. Not a thing exist here, no wind, no stone, not even a nail dare to challenge the flatness that heavily resides there. The ground is an seamingly endless, waveless pristine mirror, an ice sea with no ripples. There is apparently nothing to do or see there, so actually no demon wants to either own it or come here. No demon is known to actually live here. \nPopulation: N/A"};
 
@@ -37,7 +38,12 @@ public class HistorySelectionButtons : MonoBehaviour {
 	{"Demono","Angelum","Celticus","Nordi","Nyla","Homo"};
 
 	public static string[] GenusesDescription = new string[6] 
-	{"Demono","Angelum","Celticus","Nordi","Nyla","Homo"};
+	{"Natives of the hell circle, the Demono genus, or original demon, include three species: the Hornydi, the inccybi and the Vampyri.Their understanding of the fabric of the world allowed them to escape the hell circles and to travel through planes of existence. The term 'demon' was naturally extended to include all species that joined the hell circles after the invasion wars. Natural leaders of demonic armies, the Demonos are well rounded people with a light orientation to survival skills.",
+		"Former inhabitants of the Heaven consortium, Angeli were the first genus to be invaded by the original demons. \nThe invasion was a massacre, angels were overwhelmed by a sheer difference in number. Their world pillaged a their people enslaved, many millions years passed until their frustration disappeared and time ironically twisted their fate. \nTheir natural offensive skills and mastery of non-physical aspects of the universe led them to become great invaders in their turn. Their lobbying is highly responsible for the recent invasions of the Nordi and the current invasion on Homo.",
+		"Recluse, cryptic, uncompliant, the celticus people are a force of nature. Wild spirits, they are the only civilisation to have resisted the invasion. The demons proposed a peace treaty which led, after thousands of years, to appeased and economical relationships and eventually to freedom to move in the hell circles. With strong independent status, celticus people have nowadays settled in pretty much all of the 8 inhabited circles. The celticus have a particularly strong resistance to non-physical events",
+		"The Nordi people were a primitive yet highly social, diverse and peaceful civilization before encountering the first angels. Many species of the Nordi are forever lost in what will later be called the Plane War. The invasion highlighted for the first time the major gaps between physical and non-physical entities, and cause inumerable casualties in both sides. Almost entirely obliterated by the zealous angels, surviving Nords became enslaved to mostly angelic families.",
+		"The home of Nylas remain to date a mystery for all other genuses, and it is probably the only reason why demons have not yet openly tried to invade and/or enslave them. They can be seen from time to time passing by in the hell circles and people do not know how they travel. Due to the lack of information on them and the uneasyness it induces, demons tends not to confront or chase them out of their land. They seem to be very few in number and they do not get involved in demon's businesses.",
+		"The Homo Sapiens species is the latest life form that caught the attention of the angels and original demons. Plans for the invasion are setting up, and first intelligence stated that the homo sapiens species rely most uniquely on the physical aspect of their existence, and exhibit strong physical resistence. Through lessons learned from previous war with the Nordi, the demon federation tried to innovate and create their own genetically designed homo species with physical resistent traits to fight on equal terms."};
 
 
 
@@ -46,7 +52,9 @@ public class HistorySelectionButtons : MonoBehaviour {
 	{"Hornydi","Succubi","Vampyri","Seraph","Cherubim","Thronoi","Ankou","Cernun","Dagmus","Elfaji","Trolki","Berzica","Hor","Ana","Akep","Spectrum","Parisi","Rex"};
 
 	public static string[] SpeciesDescription = new string[18]
-	{"Hornydi","Succubi","Vampyri","Seraph","Cherubim","Thronoi","Ankou","Cernun","Dagmus","Elfaji","Trolki","Berzica","Hor","Ana","Akep","Spectrum","Parisi","Rex"};
+	{"Hornydi","Succubi","Vampyri","Seraph","Cherubim","Thronoi","Ankou","Cernun","Dagmus","Elfaji","Trolki","Berzica","Hor","Ana","Akep","Spectrum",
+		"The Celtica cerna designed a parasitic worm to take other the homo sapiens nervous system. The celticus plan was to produce spies in enemy territory to greatly fasten the invasion. The infected homo sapiens have enhances physical abilities and lowered non-physical affinities. However, the symbiosis sadly produces greener blood, which doomed to failure the original plan for an intelligence faction. The project was stopped and the infected humans were left to die. Sadly for everyone, not all of them did... And in addition to their genetically designed hate to humans, they developped a very legitimate one to the celticus cernum species.",
+		"Rex"};
 
 
 
@@ -106,6 +114,10 @@ public class HistorySelectionButtons : MonoBehaviour {
 		HistoChoiceDescription=HistorySelection.GetComponentInChildren<RectTransform> ();
 		ChoiceDisplay=HistoChoiceDescription.GetComponentInChildren<GridLayoutGroup> ();
 		for (int i=0; i<9; i++) { 	Choice[i]=ChoiceDisplay.GetComponentsInChildren<Button> () [i];}
+
+
+		for (int i=0; i<8; i++) {	HistoryChoiceDisplay [i] = GameObject.FindGameObjectWithTag ("HistoryDisplay").GetComponentsInChildren<Text> () [i];}
+
 		GetHistoryUIButtons ();
 		HistorySelection.enabled = false;
 
@@ -118,15 +130,15 @@ public class HistorySelectionButtons : MonoBehaviour {
 
 
 	// Interaction with UI Buttons
-	public void choice_1(){HistoryChoice = 1; HistorySelection.GetComponentsInChildren<Text> () [2].text = UpdateDescription ();}
-	public void choice_2(){HistoryChoice = 2; HistorySelection.GetComponentsInChildren<Text> () [2].text = UpdateDescription ();}
-	public void choice_3(){HistoryChoice = 3; HistorySelection.GetComponentsInChildren<Text> () [2].text = UpdateDescription ();}
-	public void choice_4(){HistoryChoice = 4; HistorySelection.GetComponentsInChildren<Text> () [2].text = UpdateDescription ();}
-	public void choice_5(){HistoryChoice = 5; HistorySelection.GetComponentsInChildren<Text> () [2].text = UpdateDescription ();}
-	public void choice_6(){HistoryChoice = 6; HistorySelection.GetComponentsInChildren<Text> () [2].text = UpdateDescription ();}
-	public void choice_7(){HistoryChoice = 7; HistorySelection.GetComponentsInChildren<Text> () [2].text = UpdateDescription ();}
-	public void choice_8(){HistoryChoice = 8; HistorySelection.GetComponentsInChildren<Text> () [2].text = UpdateDescription ();}
-	public void choice_9(){HistoryChoice = 9; HistorySelection.GetComponentsInChildren<Text> () [2].text = UpdateDescription ();}	
+	public void choice_1(){HistoryChoice = 1; UpdateDescription ();}
+	public void choice_2(){HistoryChoice = 2; UpdateDescription ();}
+	public void choice_3(){HistoryChoice = 3; UpdateDescription ();}
+	public void choice_4(){HistoryChoice = 4; UpdateDescription ();}
+	public void choice_5(){HistoryChoice = 5; UpdateDescription ();}
+	public void choice_6(){HistoryChoice = 6; UpdateDescription ();}
+	public void choice_7(){HistoryChoice = 7; UpdateDescription ();}
+	public void choice_8(){HistoryChoice = 8; UpdateDescription ();}
+	public void choice_9(){HistoryChoice = 9; UpdateDescription ();}	
 
 
 
@@ -138,17 +150,41 @@ public class HistorySelectionButtons : MonoBehaviour {
 
 
 
-	public static string UpdateDescription() {
+	public static void UpdateDescription() {
 		switch(currentStep){
-		case PlayerHistoryStep.HELLCIRCLE: 	return(HellCirclesDescription[HistoryChoice-1]);
-		case PlayerHistoryStep.GENUS:		return(GenusesDescription[HistoryChoice-1]);
-		case PlayerHistoryStep.SPECIES:		return(SpeciesDescription[HistoryChoice-1+3*(MenuGUI.genusSelection-1)]);
-		case PlayerHistoryStep.CLASS:		return(JobsDescription[HistoryChoice-1]);
-		case PlayerHistoryStep.ORIGIN:		return(OriginsDescription[HistoryChoice-1]);
-		case PlayerHistoryStep.TEMPER:		return(DeathStatesDescription[HistoryChoice-1]);
-		case PlayerHistoryStep.ASTRO:		return(AstrosDescription[HistoryChoice-1]);
-		case PlayerHistoryStep.AFFINITY:	return(AffisDescription[HistoryChoice-1]);
-		default: 							return(" ");
+		case PlayerHistoryStep.HELLCIRCLE: 	
+			HistorySelection.GetComponentsInChildren<Text> () [2].text =HellCirclesDescription[HistoryChoice-1];
+			HistoryChoiceDisplay [0].text=HellCircles[HistoryChoice-1];
+			break;
+		case PlayerHistoryStep.GENUS:		
+			HistorySelection.GetComponentsInChildren<Text> () [2].text =GenusesDescription[HistoryChoice-1];
+			HistoryChoiceDisplay [1].text=Genuses[HistoryChoice-1];
+			break;
+		case PlayerHistoryStep.SPECIES:		
+			HistorySelection.GetComponentsInChildren<Text> () [2].text =SpeciesDescription[HistoryChoice-1+3*(MenuGUI.genusSelection-1)];
+			HistoryChoiceDisplay [2].text=Species[HistoryChoice-1+3*(MenuGUI.genusSelection-1)];
+			break;
+		case PlayerHistoryStep.CLASS:		
+			HistorySelection.GetComponentsInChildren<Text> () [2].text =JobsDescription[HistoryChoice-1];
+			HistoryChoiceDisplay [3].text=Jobs[HistoryChoice-1];
+			break;
+		case PlayerHistoryStep.ORIGIN:		
+			HistorySelection.GetComponentsInChildren<Text> () [2].text =OriginsDescription[HistoryChoice-1];
+			HistoryChoiceDisplay [4].text=Origins[HistoryChoice-1];
+			break;
+		case PlayerHistoryStep.TEMPER:		
+			HistorySelection.GetComponentsInChildren<Text> () [2].text =DeathStatesDescription[HistoryChoice-1];
+			HistoryChoiceDisplay [5].text=DeathStates[HistoryChoice-1];
+			break;
+		case PlayerHistoryStep.ASTRO:		
+			HistorySelection.GetComponentsInChildren<Text> () [2].text =AstrosDescription[HistoryChoice-1];
+			HistoryChoiceDisplay [6].text=Astros[HistoryChoice-1];
+			break;
+		case PlayerHistoryStep.AFFINITY:	
+			HistorySelection.GetComponentsInChildren<Text> () [2].text =AffisDescription[HistoryChoice-1];
+			HistoryChoiceDisplay [7].text=Affis[HistoryChoice-1];
+			break;
+
 		}
 	}
 
