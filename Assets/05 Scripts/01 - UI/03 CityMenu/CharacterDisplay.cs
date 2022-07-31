b8908260ec35718a31ddc88a0ab3273113d98133
Rogabill Nouet <rgallouet@gmail.com>
2022-07-31 18:20:20 +0800

    Solving the libs for Android SQLite, and continuing the migration of the characters in menu

--- Assets/05 Scripts/01 - UI/03 CityMenu/CharacterDisplay.cs
+++ Assets/05 Scripts/01 - UI/03 CityMenu/CharacterDisplay.cs
@@ -38,18 +38,18 @@ public class CharacterDisplay : MonoBehaviour {
         images[12].sprite = characterInMenuSprites.GetSprite("Rank","Rank_" +       demonPartChoices.LeftFootQuality);
 
         //Characters
-        images[13].sprite = characterInMenuSprites.GetSprite("Head",             "Head_" +               demonPartChoices.HeadChoiceID);
-        images[14].sprite = characterInMenuSprites.GetSprite("Body",             "Body_" +               demonPartChoices.BodyChoiceID);
-        images[15].sprite = characterInMenuSprites.GetSprite("RightUpperArm",    "RightUpperArm_" +      demonPartChoices.RightUpperArmChoiceID);
-        images[16].sprite = characterInMenuSprites.GetSprite("RightLowerArm",    "RightLowerArm_" +      demonPartChoices.RightLowerArmChoiceID);
-        images[17].sprite = characterInMenuSprites.GetSprite("RightFist",        "RightFist_" +          demonPartChoices.RightFistChoiceID);
-        images[18].sprite = characterInMenuSprites.GetSprite("RightLeg",         "RightLeg_" +           demonPartChoices.RightLegChoiceID);
-        images[19].sprite = characterInMenuSprites.GetSprite("RightFoot",        "RightFoot_" +          demonPartChoices.RightFootChoiceID);
-        images[20].sprite = characterInMenuSprites.GetSprite("LeftUpperArm",     "LeftUpperArm_" +       demonPartChoices.LeftUpperArmChoiceID);
-        images[21].sprite = characterInMenuSprites.GetSprite("LeftLowerArm",     "LeftLowerArm_" +       demonPartChoices.LeftLowerArmChoiceID);
-        images[22].sprite = characterInMenuSprites.GetSprite("LeftFist",         "LeftFist_" +           demonPartChoices.LeftFistChoiceID);
-        images[23].sprite = characterInMenuSprites.GetSprite("LeftLeg",          "LeftLeg_" +            demonPartChoices.LeftLegChoiceID);
-        images[24].sprite = characterInMenuSprites.GetSprite("LeftFoot",         "LeftFoot_" +           demonPartChoices.LeftFootChoiceID);
+        images[13].sprite = characterInMenuSprites.GetSprite("MenuHead",             "MenuHead_" +               demonPartChoices.HeadChoiceID);
+        images[14].sprite = characterInMenuSprites.GetSprite("MenuBody",             "MenuBody_" +               demonPartChoices.BodyChoiceID);
+        images[15].sprite = characterInMenuSprites.GetSprite("MenuRightUpperArm",    "MenuRightUpperArm_" +      demonPartChoices.RightUpperArmChoiceID);
+        images[16].sprite = characterInMenuSprites.GetSprite("MenuRightLowerArm",    "MenuRightLowerArm_" +      demonPartChoices.RightLowerArmChoiceID);
+        images[17].sprite = characterInMenuSprites.GetSprite("MenuRightFist",        "MenuRightFist_" +          demonPartChoices.RightFistChoiceID);
+        images[18].sprite = characterInMenuSprites.GetSprite("MenuRightLeg",         "MenuRightLeg_" +           demonPartChoices.RightLegChoiceID);
+        images[19].sprite = characterInMenuSprites.GetSprite("MenuRightFoot",        "MenuRightFoot_" +          demonPartChoices.RightFootChoiceID);
+        images[20].sprite = characterInMenuSprites.GetSprite("MenuLeftUpperArm",     "MenuLeftUpperArm_" +       demonPartChoices.LeftUpperArmChoiceID);
+        images[21].sprite = characterInMenuSprites.GetSprite("MenuLeftLowerArm",     "MenuLeftLowerArm_" +       demonPartChoices.LeftLowerArmChoiceID);
+        images[22].sprite = characterInMenuSprites.GetSprite("MenuLeftFist",         "MenuLeftFist_" +           demonPartChoices.LeftFistChoiceID);
+        images[23].sprite = characterInMenuSprites.GetSprite("MenuLeftLeg",          "MenuLeftLeg_" +            demonPartChoices.LeftLegChoiceID);
+        images[24].sprite = characterInMenuSprites.GetSprite("MenuLeftFoot",         "MenuLeftFoot_" +           demonPartChoices.LeftFootChoiceID);
 
 
     }
